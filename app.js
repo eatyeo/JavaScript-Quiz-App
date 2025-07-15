9d4c77064f2a9ffa8f10807b2236b0d3c47fbbc6
emily <emilyatyeo44@gmail.com>
2025-07-14 21:44:31 -0400

    basic question structure initialized

---
+++ app.js
@@ -0,0 +1,13 @@
+const question = [
+    {
+        topic: 'RHOBH',
+        question: 'What is your star sign?',
+        possibleAnswers: ['Fire (Aries, Sagittarius, Leo)', 'Air (Gemini, Libra, Aquarius)', 'Earth (Taurus, Virgo, Capricorn)', 'Water (Cancer, Scorpio, Pisces)'],
+        // Need to find out final characters & tally up all individual question answers
+    },
+    {
+        topic: 'RHOBH',
+        question: '',
+        possibleAnswers: [],
+    }
+]
